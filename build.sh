b9750de9fad216cb672e4a6668bf71c6639bef9d
victorAponteParedes <vaponte520@gmail.com>
2023-02-01 21:38:54 -0300

    First commit

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+#poetry install
+
+python manage.py collectstatic --no-input
+
+pip install -r requirements.txt
+python manage.py migrate
+
---
+++ djangocrud/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from tasks import views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("signup/", views.signup, name="signup"),
+    path("tasks/", views.tasks, name="tasks"),
+    path("tasks_completed/", views.tasks_completed, name="tasks_completed"),
+    path("logout/", views.signout, name="logout"),
+    path("signin/", views.signin, name="signin"),
+    path("tasks/create/", views.create_task, name="create_task"),
+    path("tasks/<int:task_id>", views.task_detail, name="task_detail"),
+    path("tasks/<int:task_id>/complete", views.task_complete, name="task_complete"),
+    path("tasks/<int:task_id>/delete", views.task_delete, name="task_delete"),
+]
